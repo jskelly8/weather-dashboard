0c2859a027522a57244d6bccfbae57afc074ae23
Jess Skelton <jessskelton888@gmail.com>
2023-11-21 11:40:24 -0600

    Added metadata & script links and pseudocode to index.html, Added root colors to style.css, Added DOM element variables to JS.

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// DOM Elements
+let formInput = $('form-control'); // city input form
+let searchBtn = $('search-button'); // city input search button
+let cityhistory = $('city-history'); // city history section for JS list inserts under search
+let chosenCity = $('chosen-city'); // section where searched city will show city name, current date, condition icon, temp, wind speed, & humidity
+let cityForecast = $('city-forecast'); // 5-day forecast cards section showing same weather info as current day
+
