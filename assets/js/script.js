754b46111af9111d2b8c710a354f0049277a2298
Jess Skelton <jessskelton888@gmail.com>
2023-11-22 23:35:45 -0600

    restarted script.js code in order to cleanly start jquerey code -- Added event listeners to save button and saved cities & Functions to save and load to local Storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,52 @@
-// DOM Elements
-let formInput = $('form-control'); // city input form
-let searchBtn = $('search-button'); // city input search button
-let cityhistory = $('city-history'); // city history section for JS list inserts under search
-let chosenCity = $('chosen-city'); // section where searched city will show city name, current date, condition icon, temp, wind speed, & humidity
-let cityForecast = $('city-forecast'); // 5-day forecast cards section showing same weather info as current day
+// Function in order to only run once document is fully loaded and ready
+$(document).ready(function () {
+    const apiKey = 'b210e8d2620a54419160839100e5eec9'; 
 
+    // Click Event Listener on Search Button
+    $('#search-button').on('click', function () {
+
+        // Get city name from entered form input (trimming white space)
+        const cityName = $('.form-control').val().trim();
+
+        // If city name isn't empty, *then* fetch the data
+        if (cityName !== '') {
+            fetchWeather(cityName);
+        }
+    });
+
+    // Click Event Listener on saved Search History city
+    $('#city-history').on('click', 'li', function () {
+        const cityName = $(this).text(); // Pulls the city name text
+        fetchWeather(cityName);
+    });
+
+    // Function to get city's weather data
+
+    // Function to save city name to local storage
+    function saveToLocalStorage(cityName) {
+
+        // Retrieve city history from local storage, if there
+        let cityHistory = JSON.parse(localStorage.getItem('cityHistory')) || [];
+        
+        // If city name not already in local storage, add entered & update history list
+        if (!cityHistory.includes(cityName)) {
+            cityHistory.push(cityName);
+            localStorage.setItem('cityHistory', JSON.stringify(cityHistory));
+            loadCityHistory();
+        }
+    }
+
+    // Function to load & display city history list
+    function loadCityHistory() {
+
+        let cityHistory = JSON.parse(localStorage.getItem('cityHistory')) || [];
+        
+        // Update history list, if needed
+        $('#city-history').html(cityHistory.map(city => `<li>${city}</li>`).join(''));
+    }
+
+    // Load Event Listener to load & display city history list, if available
+    $(window).on('load', function () {
+        loadCityHistory();
+    });
+});
